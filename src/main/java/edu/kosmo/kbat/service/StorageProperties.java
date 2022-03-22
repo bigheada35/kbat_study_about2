5b5e50e3be51a633ffab926028040713bb14bb8d
bigheada35 <bigheada@naver.com>
2022-03-22 11:58:40 +0900

    부팅중에러증상(StorageProperties부분에 @Component를 꼭사용해야함)

--- src/main/java/edu/kosmo/kbat/service/StorageProperties.java
+++ src/main/java/edu/kosmo/kbat/service/StorageProperties.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Service;
 
-@Service
+@Component
 @ConfigurationProperties("storage")
 public class StorageProperties {
 
