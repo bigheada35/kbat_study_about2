4de21e259ac7734e7f36b22d5f1a359ccd0b4c8a
bigminikim <bigminikim@gmail.com>
2022-03-25 03:16:38 +0900

    선화님_공지사항UI_0324 머지+ myReview쿼리 수정

--- src/main/java/edu/kosmo/kbat/controller/MainController.java
+++ src/main/java/edu/kosmo/kbat/controller/MainController.java
@@ -1,31 +1,35 @@
 package edu.kosmo.kbat.controller;
 
 
+import java.io.IOException;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import edu.kosmo.kbat.joinvo.ProductProductViewVO;
 import edu.kosmo.kbat.page.Criteria;
 import edu.kosmo.kbat.page.PageVO;
-import edu.kosmo.kbat.principal.PrincipalDetails;
 import edu.kosmo.kbat.service.MainService;
-import edu.kosmo.kbat.service.MyPageService;
+
 import edu.kosmo.kbat.service.ProductService;
-import edu.kosmo.kbat.vo.MemberVO;
+import edu.kosmo.kbat.service.QBoardService;
+import edu.kosmo.kbat.service.RBoardService;
+import edu.kosmo.kbat.service.UserService;
 import edu.kosmo.kbat.vo.ProductVO;
+import edu.kosmo.kbat.vo.QBoardAndMemberVO;
+import edu.kosmo.kbat.vo.RBoardAndMemberVO;
+import edu.kosmo.kbat.vo.ReviewVO;
+import edu.kosmo.kbat.vo.UserVO;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -37,10 +41,6 @@ public class MainController {
 	final int NameLimit = 15;
 	final int DescLimit = 25;
 	
-
-	@Autowired
-	private	MyPageService myPageService;
-	
 	@Autowired
 	private	ProductService productService;
 
@@ -48,6 +48,11 @@ public class MainController {
 	@Autowired
 	private MainService mainService;
 	
+	@Autowired
+	private RBoardService rboardService;
+	
+	@Autowired
+	private QBoardService qboardService;
 	
 	@GetMapping("/pay/import")
 	public void import2() {
@@ -80,7 +85,7 @@ public class MainController {
 		model.addAttribute("products", productVO);
 	}
 	@GetMapping("/main/detail")
-	public void detail(HttpServletRequest request,  Model model) {
+	public void detail(HttpServletRequest request,  Model model, Criteria cri, RBoardAndMemberVO boardVO, ReviewVO rboardVO, QBoardAndMemberVO qboardVO) {
 		String product_id = (String) request.getParameter("product_id");
 		System.out.println("----detail----product_id:"+product_id);
 		ProductVO productVO = productService.get(Integer.valueOf(product_id));
@@ -100,7 +105,22 @@ public class MainController {
 		}
 		
 		model.addAttribute("prod", productVO);
+		
+		log.info("list()..");		
+		model.addAttribute("rlist", rboardService.rgetList(cri));
+		int total = rboardService.rgetTotalCount();
+		log.info("total" + total);
+		
+		model.addAttribute("pageMaker", new PageVO(cri, total));
+		
+		System.out.println("---------------rboardVO.getReview_id() : " + rboardVO.getReview_id());
+
+		model.addAttribute("qlist", qboardService.qgetList(cri));
+		System.out.println("member+id : " + qboardService.qgetList(cri));
+		model.addAttribute("pageMaker", new PageVO(cri, total));	
+		
 	}
+	
 	@GetMapping("/main/playVideo")
 	public String playVideo(HttpServletRequest request,  Model model) {
 		String product_id = (String) request.getParameter("product_id");
@@ -121,7 +141,7 @@ public class MainController {
 		mav.addObject("pageMaker", new PageVO(cri, total));
 		return mav;
 	}
-		@GetMapping("/main/cartorder")
+	@GetMapping("/main/cartorder")
 	public String cartorder(HttpServletRequest request,  Model model) {
 		String product_id = (String) request.getParameter("product_id");
 		System.out.println("----cartorder----product_id:"+product_id);
@@ -139,5 +159,4 @@ public class MainController {
 		
 		return "/pay/checkout";
 	}
-	
 }
--- src/main/webapp/WEB-INF/views/nboard/list.jsp
+++ src/main/webapp/WEB-INF/views/nboard/list.jsp
@@ -12,77 +12,62 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>Insert title here</title>
 </head>
-<body>  
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col">번호</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
-
-   <table width="500" cellpadding="0" cellspacing="0" border="1">
-      <tr>
-         <td>번호</td>
-         <td>제목</td>
-         <td>작성자</td>
-         <td>작성일자</td>
-         <td>조회수</td>
-      </tr>
-      <c:forEach items="${list}" var="board">
-      <tr>
-         <td>${board.board_id}</td>
-         <td>
-            <a href="ncontent_view?board_id=${board.board_id}">${board.board_title}</a></td>
-         <td>${board.member_id}</td>
-         <td>${board.board_date}</td>
-         <td>${board.board_hit}</td>
-      </tr>
-      </c:forEach>                
-   </table>
-   
-   <tr>
-   		<c:if test="${principal.user.member_id == 'kbatc5'}" >
-			<td colspan="5"> <a href="nwrite_view">글작성</a> </td>
-	    </c:if>	
-   </tr>  
-   <br>
-  
-   <c:if test="${pageMaker.pre}">
-         <a href="nlist${pageMaker.makeQuery(pageMaker.startPage - 1) }">«</a>
-   </c:if>
-
-	<!-- 링크를 걸어준다 1-10페이지까지 페이지를 만들어주는것  -->
-	<c:forEach var="idx" begin="${pageMaker.startPage }" end="${pageMaker.endPage }" >
-		<a href="nlist${pageMaker.makeQuery(idx)}">${idx}</a>
-	</c:forEach>
-	      
-	<c:if test="${pageMaker.next && pageMaker.endPage > 0}">
-		<a href="nlist${pageMaker.makeQuery(pageMaker.endPage +1) }"> » </a>
-	</c:if> <br>
-
+<style>
+	.paging{
+		text-align : center;
+	}
+	
+	.n-button{
+		position : 
+	}
+</style>
+<body>
+	<br>
+	<div class="container">  
+		<table class="table">  
+			<h1>공지사항</h1>   
+			<thead> 
+				<tr>
+				<th scope="col">번호</th>
+				<th scope="col">제목</th>
+				<th scope="col">작성자</th>
+				<th scope="col" style="text-align:center">작성일자</th>
+				<th scope="col" style="text-align:center">조회수</th>
+				</tr>
+			</thead>
+			<tbody>	
+				<c:forEach items="${list}" var="board">
+					<tr>
+						<td>${board.board_id}</td>
+						<td>
+						<a href="ncontent_view?board_id=${board.board_id}" style="text-decoration:none; color:black">${board.board_title}</a></td>
+						<td>${board.member_id}</td>
+						<td style="text-align:center">${board.board_date}</td>
+						<td style="text-align:center">${board.board_hit}</td>
+					</tr>
+				</c:forEach>       
+			</tbody>
+		<br>
+		</table>
+		<div class="n-button">
+			<tr>
+				<c:if test="${principal.user.member_id == 'kbatc5'}" >
+					<a href="nwrite_view"><button type="button" class="btn btn-dark">공지사항 등록</button></a>
+				</c:if>	
+			</tr>  
+		</div>
+		
+		<div class="paging">		
+			<c:if test="${pageMaker.pre}">
+				<a href="nlist${pageMaker.makeQuery(pageMaker.startPage - 1) }">«</a>
+			</c:if>		   
+			<c:forEach var="idx" begin="${pageMaker.startPage }" end="${pageMaker.endPage }">
+				<a href="nlist${pageMaker.makeQuery(idx)}" style="text-decoration:none; color:black">${idx}&nbsp;&nbsp;</a>
+			</c:forEach>			      
+			<c:if test="${pageMaker.next && pageMaker.endPage > 0}">
+				<a href="nlist${pageMaker.makeQuery(pageMaker.endPage +1) }"> » </a>
+			</c:if> 
+		</div>		
+	<div>
 </body>
-
 </html>
